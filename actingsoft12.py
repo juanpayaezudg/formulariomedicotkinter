379b2234699c23b36ca8db68a54c99dd2e79b515
Juan <yanezgonzalezjuanpablo@gmail.com>
2023-09-17 22:31:58 -0600

    version final

--- actingsoft12.py
+++ actingsoft12.py
@@ -11,17 +11,23 @@ root.resizable(False, False)
 root.configure(bg="#326273")
 
 lista_pacientes = []
-lista_imc = []
-
 
 class Paciente:
-    def __init__(self, codigo, nombre, ap, am, edad, genero):
+    def __init__(self, codigo, nombre, ap, am, edad, genero, peso, altura):
         self.codigo = codigo
         self.nombre = nombre
         self.ap = ap
         self.am = am
         self.edad = edad
         self.genero = genero
+        self.peso = float(peso)
+        self.altura = float(altura) / 100  
+        
+    def calcular_imc(self):
+        if self.altura > 0:
+            return self.peso / (self.altura ** 2)
+        else:
+            return 0
 
 def enviar():
     codigo = codValor.get()
@@ -30,17 +36,14 @@ def enviar():
     am = amValor.get()
     edad = edadValor.get()
     genero = gen_combobox.get()
+    peso = pesoValor.get()
+    altura = alturaValor.get()
 
-    paciente = Paciente(codigo, nombre, ap, am, edad, genero)
+    paciente = Paciente(codigo, nombre, ap, am, edad, genero, peso, altura)
 
   
     lista_pacientes.append(paciente)
     print(paciente)
-
-pesoValor = tk.Entry(root, width=5, bd=2, font=20)
-pesoValor.place(x=260, y=270)
-alturaValor = tk.Entry(root, width=5, bd=2, font=20)
-alturaValor.place(x=500, y=270)   
     
 
 def limpiar():
@@ -59,35 +62,48 @@ def buscar():
     ventana_emergente.configure(bg="#326273")
     ventana_emergente.title("Sección Buscar")
     
-    try:
-        codigo = int(codValor.get())  # Convertimos el valor recogido a entero
-    except ValueError:
-        messagebox.showerror('Error', 'El ID ingresado no es válido.')
-        return
-
-    paciente_encontrado = None
-
-    # Buscamos al paciente en la lista de pacientes
-    for paciente in lista_pacientes:
-        if paciente.codigo == codigo:  # Asegúrate de que el ID del paciente también sea entero
-            paciente_encontrado = paciente
-            break
-
-    # Mostramos los datos del paciente si lo encontramos
-    if paciente_encontrado:
-        info = f'''
-        ID: {paciente_encontrado.codigo}
-        Nombre: {paciente_encontrado.nombre}
-        Apellido Paterno: {paciente_encontrado.ap}
-        Apellido Materno: {paciente_encontrado.am}
-        Edad: {paciente_encontrado.edad}
-        Género: {paciente_encontrado.genero}
-        '''
-        messagebox.showinfo('Información del paciente', info)
-    else:
-        messagebox.showerror('Error', 'Paciente con ese ID no encontrado')
+    paciente_actual = None 
     
-    Label(ventana_emergente, text='ID', font=23, bg="#326273", fg="#fff").place(x=50, y=50)
+    def realizar_busqueda():
+        nonlocal paciente_actual
+        id_a_buscar = iDbuscar.get()
+        for paciente in lista_pacientes:
+            if paciente.codigo == id_a_buscar:
+                paciente_actual = paciente
+                nomValor.delete(0, 'end')
+                nomValor.insert(0, paciente.nombre)
+                apValor.delete(0, 'end')
+                apValor.insert(0, paciente.ap)
+                amValor.delete(0, 'end')
+                amValor.insert(0, paciente.am)
+                edadValor.delete(0, 'end')
+                edadValor.insert(0, paciente.edad)
+                pesoValor.delete(0, 'end')
+                pesoValor.insert(0, paciente.peso)
+                alturaValor.delete(0, 'end')
+                alturaValor.insert(0, paciente.altura)
+                imc = paciente_actual.calcular_imc()
+                imcLabel.config(text=f"IMC: {imc:.2f}")
+                gen_combobox.set(paciente.genero)
+                return
+        messagebox.showinfo("Búsqueda", "No se encontró paciente con ese ID")
+
+    def modificar_paciente():
+        nonlocal paciente_actual
+        if paciente_actual:
+            paciente_actual.nombre = nomValor.get()
+            paciente_actual.ap = apValor.get()
+            paciente_actual.am = amValor.get()
+            paciente_actual.edad = edadValor.get()
+            paciente_actual.genero = gen_combobox.get()
+            paciente_actual.peso = pesoValor.get()
+            paciente_actual.altura = alturaValor.get()
+            messagebox.showinfo("Modificación", "Datos del paciente actualizados")
+        else:
+            messagebox.showwarning("Atención", "Primero busca y selecciona un paciente")
+        
+    Label(ventana_emergente, text="Por favor ingresa el ID del paciente a buscar: ", font="arial 13", bg="#326273", fg="#fff").place(x=20, y=20)
+    
     Label(ventana_emergente, text='Nombre', font=23, bg="#326273", fg="#fff").place(x=50, y=100)
     Label(ventana_emergente, text='Apellido paterno', font=23, bg="#326273", fg="#fff").place(x=40, y=150)
     Label(ventana_emergente, text='Apellido materno', font=23, bg="#326273", fg="#fff").place(x=40, y=200)
@@ -96,25 +112,36 @@ def buscar():
 
     Label(ventana_emergente, text='Peso en kg', font=23, bg="#326273", fg="#fff").place(x=140, y=270)
     Label(ventana_emergente, text='Altura en cm', font=23, bg="#326273", fg="#fff").place(x=360, y=270)
+    imcLabel = Label(ventana_emergente, text='IMC: -', font=23, bg="#326273", fg="#fff")
+    imcLabel.place(x=290, y=300)
+
 
 # entrada de datos
 
 #cajas
+    iDbuscar = tk.Entry(ventana_emergente, width=12, bd=2, font=20)
+    iDbuscar.place(x=400, y=20)
     nomValor = tk.Entry(ventana_emergente, width=12, bd=2, font=20)
-    nomValor.place(x=200, y=50)
+    nomValor.place(x=200, y=100)
     apValor = tk.Entry(ventana_emergente, width=12, bd=2, font=20)
     apValor.place(x=200, y=150)
     amValor = tk.Entry(ventana_emergente, width=12, bd=2, font=20)
     amValor.place(x=200, y=200)
-    codValor = tk.Entry(ventana_emergente, width=10, bd=2, font=20)
-    codValor.place(x=200, y=100)
     edadValor = tk.Entry(ventana_emergente, width=5, bd=2, font=20)
     edadValor.place(x=440, y=100)
+    pesoValor = tk.Entry(ventana_emergente, width=5, bd=2, font=20)
+    pesoValor.place(x=260, y=270)
+    alturaValor = tk.Entry(ventana_emergente, width=5, bd=2, font=20)
+    alturaValor.place(x=500, y=270)
 
 # combobox género
     gen_combobox = Combobox(ventana_emergente, values=['Masculino', 'Femenino'], font='arial 14', state='r', width=12)
     gen_combobox.place(x=440, y=200)
-    gen_combobox.set('Masculino')
+    
+    Button(ventana_emergente, text="Buscar", bg="#326273", fg="white", width=15, height=2, command=realizar_busqueda).place(x=550, y=20)
+    Button(ventana_emergente, text="Guardar", bg="#326273", fg="white", width=15, height=2, command=modificar_paciente).place(x=200, y=350)
+    Button(ventana_emergente, text="Salir", bg="#326273", fg="white", width=15, height=2, command=lambda: ventana_emergente.destroy()).place(x=540, y=350)
+    
 
 
 Label(root, text="Por favor llena este formulario: ", font="arial 13", bg="#326273", fg="#fff").place(x=20, y=20)
@@ -133,27 +160,30 @@ Label(root, text='Altura en cm', font=23, bg="#326273", fg="#fff").place(x=360, 
 # entrada de datos
 
 #cajas
-nomValor = tk.Entry(root, width=12, bd=2, font=20)
-nomValor.place(x=200, y=50)
+codValor = tk.Entry(root, width=12, bd=2, font=20)
+codValor.place(x=200, y=50)
+nomValor = tk.Entry(root, width=10, bd=2, font=20)
+nomValor.place(x=200, y=100)
 apValor = tk.Entry(root, width=12, bd=2, font=20)
 apValor.place(x=200, y=150)
 amValor = tk.Entry(root, width=12, bd=2, font=20)
 amValor.place(x=200, y=200)
-codValor = tk.Entry(root, width=10, bd=2, font=20)
-codValor.place(x=200, y=100)
 edadValor = tk.Entry(root, width=5, bd=2, font=20)
 edadValor.place(x=440, y=100)
+pesoValor = tk.Entry(root, width=5, bd=2, font=20)
+pesoValor.place(x=260, y=270)
+alturaValor = tk.Entry(root, width=5, bd=2, font=20)
+alturaValor.place(x=500, y=270)
 
 # combobox género
 gen_combobox = Combobox(root, values=['Masculino', 'Femenino'], font='arial 14', state='r', width=12)
 gen_combobox.place(x=440, y=200)
-gen_combobox.set('Masculino')
 
 
 
 Button(root, text="Guardar", bg="#326273", fg="white", width=15, height=2, command=enviar).place(x=100, y=350)
 Button(root, text="Limpiar", bg="#326273", fg="white", width=15, height=2, command=limpiar).place(x=390, y=350)
 Button(root, text="Salir", bg="#326273", fg="white", width=15, height=2, command=lambda: root.destroy()).place(x=540, y=350)
-Button(root, text="Buscar", bg="#326273", fg="white", width=15, height=2, command=buscar).place(x=100, y=420)
+Button(root, text="Buscar", bg="#326273", fg="white", width=15, height=2, command=buscar).place(x=250, y=350)
 
 root.mainloop()
